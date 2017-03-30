77c97523c261d3b9b2f4f5845b4009bcc3dbb64a
Samir <photography.sam@gmail.com>
2017-03-30 10:56:14 +0800

    Empty project with authentication enabled

---
+++ EventsPlannerMvc/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="EventsPlannerMvc.MvcApplication" Language="C#" %>
---
+++ EventsPlannerMvc/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(EventsPlannerMvc.Startup))]
+namespace EventsPlannerMvc
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
